c00adbbd2bfe58d75921cb130ea445c9a51a7f65
Laura Raquel Sarmiento Deaza <lraquelsarmientod@gmail.com>
2026-04-30 06:51:08 -0500

    2

--- song.py
+++ song.py
@@ -1,8 +1,8 @@
 # Write your code here!
 class song:
     def __init__(self, name, artist, length):
-        self.song = name
-        self.color = artist
-        self.song = length
-        my_song = Song("tv off", "Kendrick Lamar", 3.7)
-print(my_song.get_length_in_seconds())
+        self.name = name
+        self.artist = artist
+        self.length = length
+    def get_length_in_seconds(self):
+        return self.length * 60
